9ae9a9cbaf986d0242bb3f9a31defd58acdb913b
Андрей Коваленко < >
2016-07-29 15:32:46 +0300

    reference replace bug fixed

--- sort.h
+++ sort.h
@@ -16,23 +16,60 @@ namespace mtc
   template<class _iterator, class T, class compare>
   void  __inplace_sort__( _iterator first, _iterator last, const T& pv, compare cmp )
   {
-      T*        v = (first + ((last - first)/2));
+      const T*  m = first + (last - first)/2;
       _iterator f = first;
       _iterator l = last;
 
       do
       {
-        while ( f < last  && cmp( *f, *v ) < 0 ) ++f;
-        while ( l > first && cmp( *v, *l ) < 0 ) --l;
+        while ( f < last  && cmp( *f, *m ) < 0 ) ++f;
+        while ( l > first && cmp( *m, *l ) < 0 ) --l;
 
         if ( f <= l )
+        {
+          m = m == f ? l :
+              m == l ? f : m;
           __inplace_swap__( *f++, *l-- );
+        }
       } while ( f <= l );
 
       if ( l > first )
-        __inplace_sort__( first, l, *v, cmp );
+        __inplace_sort__( first, l, pv, cmp );
       if ( last > f )
-        __inplace_sort__( f, last, *v, cmp );
+        __inplace_sort__( f, last, pv, cmp );
+  }
+
+  template<class _iterator, class T>
+  void  __inplace_sort__( _iterator first, _iterator last, const T& pv )
+  {
+      const T*  m = first + (last - first)/2;
+      _iterator f = first;
+      _iterator l = last;
+
+      do
+      {
+        while ( f < last  && *f < *m ) ++f;
+        while ( l > first && *m < *l ) --l;
+
+        if ( f <= l )
+        {
+          m = m == f ? l :
+              m == l ? f : m;
+          __inplace_swap__( *f++, *l-- );
+        }
+      } while ( f <= l );
+
+      if ( l > first )
+        __inplace_sort__( first, l, pv );
+      if ( last > f )
+        __inplace_sort__( f, last, pv );
+  }
+
+  template<typename _iterator>
+  void  inplace_sort(_iterator first, _iterator last )
+  {
+    if ( first < --last )
+      __inplace_sort__( first, last, *first );    
   }
 
   template<typename _iterator, typename compare>
