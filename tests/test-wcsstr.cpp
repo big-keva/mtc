0fb08b70b6606bef98c6929d0e40273b2d22be78
Andrew Kovalenko aka Keva <keva@meta.ua>
2019-02-28 16:08:02 +0300

    tests relocated

--- tests/test-wcsstr.cpp
+++ tests/test-wcsstr.cpp
@@ -3,7 +3,7 @@
   обеспечивающий достаточное количество текста, чтобы никто не ошибся с кодировкой.
 */
 # define CATCH_CONFIG_MAIN
-# include <catch.hpp>
+# include <catch2/catch.hpp>
 # include <mtc/wcsstr.h>
 
 class fake_allocator
