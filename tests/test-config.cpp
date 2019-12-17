81a5b604182f308de823c3f398e3ebb929cdbe07
Andrew Kovalenko aka Keva <keva@meta.ua>
2019-12-17 16:47:08 +0300

    newly added

---
+++ tests/test-config.cpp
@@ -0,0 +1,15 @@
+﻿# include "../config.h"
+# include <cstdio>
+
+int main()
+{
+  auto  config = mtc::config::Open( "examples/test-config.json" );
+  auto  file_1 = config.get_path( "file_1" );
+    fprintf( stdout, "@1=%s\n", file_1.c_str() );
+  auto  file_2 = config.get_path( "file_2" );
+    fprintf( stdout, "@2=%s\n", file_2.c_str() );
+  auto  file_3 = config.get_path( "file_3" );
+    fprintf( stdout, "@3=%s\n", file_3.c_str() );
+
+  return 0;
+}
