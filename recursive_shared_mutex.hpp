e226fa74e3bfdf5f17f3f564acea85be0817d3c6
Keva <keva@meta.ua>
2020-12-09 12:38:08 +0300

    add recursive enter for read

--- recursive_shared_mutex.hpp
+++ recursive_shared_mutex.hpp
@@ -1,3 +1,54 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2000-2016 Андрей Коваленко aka Keva
+  keva@meta.ua
+  keva@rambler.ru
+  skype: big_keva
+  phone: +7(495)648-4058, +7(916)015-5592
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=============================================================================
+
+Данная лицензия разрешает лицам, получившим копию данного программного обеспечения
+и сопутствующей документации (в дальнейшем именуемыми «Программное Обеспечение»),
+безвозмездно использовать Программное Обеспечение без ограничений, включая неограниченное
+право на использование, копирование, изменение, слияние, публикацию, распространение,
+сублицензирование и/или продажу копий Программного Обеспечения, а также лицам, которым
+предоставляется данное Программное Обеспечение, при соблюдении следующих условий:
+
+Указанное выше уведомление об авторском праве и данные условия должны быть включены во
+все копии или значимые части данного Программного Обеспечения.
+
+ДАННОЕ ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ ПРЕДОСТАВЛЯЕТСЯ «КАК ЕСТЬ», БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ,
+ЯВНО ВЫРАЖЕННЫХ ИЛИ ПОДРАЗУМЕВАЕМЫХ, ВКЛЮЧАЯ ГАРАНТИИ ТОВАРНОЙ ПРИГОДНОСТИ,
+СООТВЕТСТВИЯ ПО ЕГО КОНКРЕТНОМУ НАЗНАЧЕНИЮ И ОТСУТСТВИЯ НАРУШЕНИЙ, НО НЕ ОГРАНИЧИВАЯСЬ
+ИМИ.
+
+НИ В КАКОМ СЛУЧАЕ АВТОРЫ ИЛИ ПРАВООБЛАДАТЕЛИ НЕ НЕСУТ ОТВЕТСТВЕННОСТИ ПО КАКИМ-ЛИБО ИСКАМ,
+ЗА УЩЕРБ ИЛИ ПО ИНЫМ ТРЕБОВАНИЯМ, В ТОМ ЧИСЛЕ, ПРИ ДЕЙСТВИИ КОНТРАКТА, ДЕЛИКТЕ ИЛИ ИНОЙ
+СИТУАЦИИ, ВОЗНИКШИМ ИЗ-ЗА ИСПОЛЬЗОВАНИЯ ПРОГРАММНОГО ОБЕСПЕЧЕНИЯ ИЛИ ИНЫХ ДЕЙСТВИЙ
+С ПРОГРАММНЫМ ОБЕСПЕЧЕНИЕМ.
+
+*/
 # pragma once
 # if !defined( __mtc_mutex__ )
 # define __mtc_mutex__
@@ -38,24 +89,16 @@ namespace mtc
   class shared_lock
   {
   public:
-    shared_lock(): pmtx( nullptr ), owns( false )
-      {
-      }
-    shared_lock( Mtx& mtx ): pmtx( &mtx ), owns( true )
-      {
-        mtx.lock_shared();
-      }
-	  shared_lock( Mtx& mtx, std::defer_lock_t ): pmtx( &mtx ), owns( false )
-		  {
-		  }
-	  shared_lock( Mtx& mtx, std::adopt_lock_t ): pmtx( &mtx ), owns( true )
-		  {
-		  }
-    shared_lock( shared_lock&& s ) noexcept: pmtx( s.pmtx ), owns( s.owns )
-      {
-        s.pmtx = nullptr;
-        s.owns = false;
-      }
+    shared_lock():
+      pmtx( nullptr ), owns( false ) {}
+    shared_lock( Mtx& mtx ):
+      pmtx( &mtx ), owns( true )  {  mtx.lock_shared();  }
+	  shared_lock( Mtx& mtx, std::defer_lock_t ):
+	    pmtx( &mtx ), owns( false ) {}
+	  shared_lock( Mtx& mtx, std::adopt_lock_t ):
+	    pmtx( &mtx ), owns( true )  {}
+    shared_lock( shared_lock&& s ) noexcept:
+      pmtx( s.pmtx ), owns( s.owns )  {  s.pmtx = nullptr;  s.owns = false;  }
    ~shared_lock()
       {
         if ( pmtx != nullptr && owns )
@@ -122,8 +165,8 @@ namespace mtc
   inline
   void  recursive_shared_mutex::lock()
     {
-      std::unique_lock<std::recursive_mutex>  u_lock( thrdLock );
-      auto                                    thrdId = std::this_thread::get_id();
+      auto  u_lock = make_unique_lock( thrdLock );
+      auto  thrdId = std::this_thread::get_id();
 
       if ( thrdId != writerId )
         w_Writer.wait( u_lock, [&](){  return nWriters == 0;  } );
@@ -137,7 +180,7 @@ namespace mtc
   inline
   void  recursive_shared_mutex::unlock()
     {
-      std::lock_guard<std::recursive_mutex> aulock( thrdLock );
+      auto  aulock = make_unique_lock( thrdLock );
 
       if ( --nWriters == 0 )
         w_Writer.notify_all();
@@ -146,20 +189,20 @@ namespace mtc
   inline
   void  recursive_shared_mutex::lock_shared()
     {
-      std::unique_lock<std::recursive_mutex>  u_lock( thrdLock );
+      auto  u_lock = make_unique_lock( thrdLock );
+      auto  thrdId = std::this_thread::get_id();
 
-      w_Writer.wait( u_lock, [&](){  return nReaders < INT_MAX && (nWriters == 0 || nReaders > 0);  } );
-        ++nReaders;
+      if ( thrdId != writerId )
+        w_Writer.wait( u_lock, [&](){  return nReaders < INT_MAX && (nWriters == 0 || nReaders > 0);  } );
+      ++nReaders;
     }
 
   inline
   void  recursive_shared_mutex::unlock_shared()
     {
-      std::lock_guard<std::recursive_mutex> aulock( thrdLock );
+      auto  aulock = make_unique_lock( thrdLock );
 
-      --nReaders;
-
-      if ( nWriters != 0 )
+      if ( --nReaders, nWriters != 0 )
       {
       // writer is waiting, and this thread is a last reader
         if ( nReaders == 0 )
