f33024ae9e96aa2ea5fc42540df6d88942cbb12f
Andrey Kovalenko aka Keva, @office <andrey.kovalenko@myoffice.team>
2021-06-08 19:48:23 +0300

    home works

--- src/zval.cpp
+++ src/zval.cpp
@@ -492,7 +492,7 @@ namespace mtc
 
         case zval::z_zmap:    return compare::test( _1, *_2.get_zmap() );
 
-        case zval::z_array_char:    return compare::test( _1, *_2.get_char() );
+        case zval::z_array_char:    return compare::test( _1, *_2.get_array_char() );
         case zval::z_array_byte:    return compare::test( _1, *_2.get_array_byte() );
         case zval::z_array_int16:   return compare::test( _1, *_2.get_array_int16() );
         case zval::z_array_word16:  return compare::test( _1, *_2.get_array_int32() );
