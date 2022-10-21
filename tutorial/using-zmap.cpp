a4afea3d1bea7b28044b2e3058b7a03bf40a623b
Andrey.Kovalenko <andrey.kovalenko@myoffice.team>
2022-10-21 11:39:49 +0300

    balls alignment

--- tutorial/using-zmap.cpp
+++ tutorial/using-zmap.cpp
@@ -40,40 +40,44 @@ void  FillZmapWithTypedAPI()
   fprintf( mtc::json::Print( stderr, m, mtc::json::print::decorated() ), "\n" );
 }
 
-/*
- * В этом примере мы создаём простой zmap по шагам, ключ за ключом, сериализуем в массив, после чего
- * загружаем и убеждаемся, что получили точно такой же zmap.
- *
- * Для этого запрашиваем значения полей и сравниваем с оригинальными
- */
-void  CreateSimpleZmapAndSerializeItToBuffer()
+void  FillZmapWithInitializerList()
 {
-  mtc::zmap         zmap;
-  mtc::zmap         znew;
-  std::vector<char> buff;
+  auto  m1 = mtc::zmap{
+    { "integer field", 32 },
+    { "long integer feeld", (int64_t)64 },
+    { "string field", "field value" },
+    { "zmap field", mtc::zmap{
+      { "zmap field 1", 1 },
+      { "zmap field 2", "2" } } },
+    { "array of integer values", mtc::array_int32{ 1, 2, 3 } },
+    { "array of float values", mtc::array_float{ 1, 2, 3 } },
+    { "array of double values", mtc::array_double{ 1, 2, 3 } },
+    { "array of string values", mtc::array_charstr{ "1", "2", "3" } },
+    { "array of any value", mtc::array_zval{ 1, 2.0, "3" } },
+    { "array of zmap", mtc::array_zmap{
+      { { 1U, 1 } },
+      { { 2U, 2.0 } },
+      { { "3", "3.0" } } } } };
+  auto  m2 = mtc::zmap{ m1, {
+    { "one_more_key", mtc::zmap{
+      { "add", "value " } } } } };
 
-  zmap.set_int32( "key as string", 32 );    // ключ - строка
-  zmap.set_charstr( 32U, "string value" );  // ключ - unsigned
-  zmap.set_float( "float value", 1.71 );
- *zmap.set_float( "other float" ) = 2.82;
-
-  zmap.set_array_charstr( "string array", { "s1", "s2", "s3" } );
-
-  buff.resize( zmap.GetBufLen() );    // резервируем место для сериализации
-  zmap.Serialize( buff.data() );
-
-  znew.FetchFrom( (const char*)buff.data() );
-
-  assert( *znew.get_int32( "key as string" ) == *zmap.get_int32( "key as string" ) );
+  fprintf( mtc::json::Print( stderr, m1, mtc::json::print::decorated() ), "\n" );
+  fprintf( mtc::json::Print( stderr, m2, mtc::json::print::decorated() ), "\n" );
 }
 
-/*
- * zmap может создаваться сразу со списком полей, как initializer_list<>, а также можно использовать
- * оператор-помощник [] с ключами в ряде ситуаций.
- *
- * В финале построенный zmap печатается как json.
- */
-void  CreateZmapAsInitializerListAndShowHowToUseZmapsAsFields()
+void  FillZmapWithSweetyAPI()
+{
+  auto  m = mtc::zmap();
+
+  m["add field"] = 19U;
+  m["add array"] = mtc::array_charstr{ "s1", "s2", "s3" };         // mtc::array_charstr и
+  m["add vector"] = std::vector<std::string>{ "s1", "s2", "s3" };  // std::vector<std::string> - это синонимы
+
+  fprintf( mtc::json::Print( stderr, m, mtc::json::print::decorated() ), "\n" );
+}
+
+void  TestTypedFieldAccess()
 {
   auto  zmap = mtc::zmap{
     { "integer field", 32 },
@@ -90,22 +94,26 @@ void  CreateZmapAsInitializerListAndShowHowToUseZmapsAsFields()
     { "array of zmap", mtc::array_zmap{
       { { 1U, 1 } },
       { { 2U, 2.0 } },
-      { { "3", "3.0" } } } }
-  };
+      { { "3", "3.0" } } } } };
 
-  zmap["add field"] = 19U;
-  zmap["add array"] = mtc::array_charstr{ "s1", "s2", "s3" };         // mtc::array_charstr и
-  zmap["add vector"] = std::vector<std::string>{ "s1", "s2", "s3" };  // std::vector<std::string> - это синонимы
+  auto  pint = zmap.get_int32( "integer field" );
+    assert( pint != nullptr );
+    assert( *pint == 32 );
 
-  mtc::json::Print( stdout, zmap, mtc::json::print::decorated() );
+    assert( zmap.get_int64( "integer field" ) == nullptr );
+    assert( zmap.get_charstr( "integer field" ) == nullptr );
+
+  auto  parr = zmap.get_array_zval( "array of any value" );
+    assert( parr != nullptr );
+    assert( zmap.get_array_double( "array of any value" ) == nullptr );
 }
 
 /*
  * Доступ к элементам zmap тоже может быть по []
  */
-void  TestZmapFieldsAccess()
+void  TestEasyFieldsAccess()
 {
-  auto  zmap = new mtc::zmap{
+  auto  zmap = mtc::zmap{
     { "integer field", 32 },
     { "long integer field", (int64_t)64 },
     { "string field", "field value" },
@@ -122,20 +130,10 @@ void  TestZmapFieldsAccess()
       { { "3", "3.0" } } } }
   };
 
-  /*
-  if ( is_set( (*zmap)["long integer field"] ) && (*zmap)["long integer field"] == 64 )
+  if ( is_set( zmap["integer field"] ) && zmap["integer field"] == 32 )
   {
-    auto p = (*zmap)["int"];
-
-    delete zmap;
-    if ( p == 64 )
-    {
-
-    int i = 0;
     //auto  ft = get_type( zmap["integer_field"] );
-    }
   }
- */
 }
 
 class Schema
@@ -209,6 +207,11 @@ void  TestParseJson()
 int   main()
 {
   FillZmapWithTypedAPI();
+  FillZmapWithInitializerList();
+  FillZmapWithSweetyAPI();
+
+  TestTypedFieldAccess();
+  TestEasyFieldsAccess();
 /*  CreateSimpleZmapAndSerializeItToBuffer();
   CreateZmapAsInitializerListAndShowHowToUseZmapsAsFields();
   TestZmapFieldsAccess();
