a9afd13821d6b6562fc2a74e9ca856b3a094a263
Keva <keva@rambler.ru>
2025-03-06 10:39:19 +0300

    prevent allocation warnings

--- src/zipStream.cpp
+++ src/zipStream.cpp
@@ -1,4 +1,6 @@
 # include "../zipStream.h"
+# include "../fileStream.h"
+# include "../recursive_shared_mutex.hpp"
 # include <vector>
 # include <mutex>
 # include <fcntl.h>
@@ -6,16 +8,11 @@
 # define ZLIB_WINAPI
 # include <zlib.h>
 
-namespace mtc
-{
-  class ZipBuffer;
-  class ZipStream;
+namespace mtc {
+namespace zip {
 
-  class ZipBuffer final: public IByteBuffer, protected std::vector<char>
+  class ZipBuffer final: public IByteBuffer, public std::vector<char>
   {
-    friend IByteBuffer*  LoadZipBuffer( const char* sz );
-    friend class ZipStream;
-
     implement_lifetime_control
 
   public:
@@ -29,20 +26,14 @@ namespace mtc
 
   };
 
+  template <class error>
   class ZipStream final: public IFlatStream
   {
-    friend IFlatStream*  OpenZipStream( const char* sz, unsigned dwmode, unsigned buflen );
-    friend IByteBuffer*  LoadZipBuffer( const char* sz );
-
     implement_lifetime_control
 
   public:     // construction
     ZipStream( gzFile p = nullptr ): gzfile( p ) {}
-   ~ZipStream()
-      {
-        if ( gzfile != nullptr )
-          gzclose( gzfile );
-      }
+   ~ZipStream();
 
   public:     // from IByteStream
     word32_t  Get(       void*, word32_t ) override;
@@ -53,7 +44,7 @@ namespace mtc
     word32_t  PosGet(       void*,   int64_t, word32_t ) override;
     word32_t  PosPut( const void*,   int64_t, word32_t ) override;
     int64_t   Seek  ( int64_t                          ) override;
-    int64_t   Size  (                                  ) override;
+    int64_t   Size  (                                  ) override {  return -1;  }
     int64_t   Tell  (                                  ) override;
 
   protected:  // variables
@@ -64,131 +55,171 @@ namespace mtc
 
   // ZipStream implementation
 
-  word32_t  ZipStream::Get( void* p, word32_t l )
+  template <class error>
+  ZipStream<error>::~ZipStream()
   {
-    std::lock_guard<std::mutex> locker( gzlock );
-
-    if ( gzfile == nullptr )
-      throw std::logic_error( "uninitialized zip stream" );
-    return (word32_t)gzread( gzfile, p, l );
+    if ( gzfile != nullptr )
+      gzclose( gzfile );
   }
 
-  word32_t  ZipStream::Put( const void* p, word32_t l )
+  template <class error>
+  word32_t  ZipStream<error>::Get( void* p, word32_t l )
   {
-    std::lock_guard<std::mutex> locker( gzlock );
-
-    if ( gzfile == nullptr )
-      throw std::logic_error( "uninitialized zip stream" );
-    return (word32_t)gzwrite( gzfile, p, l );
+    return interlocked( make_unique_lock( gzlock ),[&]()
+      {  return (word32_t)gzread( gzfile, p, l );  } );
   }
 
-  int       ZipStream::GetBuf( IByteBuffer** buf, int64_t pos, word32_t len )
+  template <class error>
+  word32_t  ZipStream<error>::Put( const void* p, word32_t l )
+  {
+    return interlocked( make_unique_lock( gzlock ),[&]()
+      {  return (word32_t)gzwrite( gzfile, p, l );  } );
+  }
+
+  template <class error>
+  int   ZipStream<error>::GetBuf( IByteBuffer** buf, int64_t pos, word32_t len )
   {
     api<ZipBuffer>  palloc;
 
-    if ( gzfile == nullptr )
-      throw std::logic_error( "uninitialized zip stream" );
     if ( buf == nullptr )
-      return EINVAL;
+      return error()( EINVAL, std::invalid_argument( "invalid (NULL) output address" ) );
 
-    if ( (palloc = allocate<ZipBuffer>( len )) == nullptr )
-      return ENOMEM;
+    try
+    {
+      palloc = new ZipBuffer( len );
 
-    if ( PosGet( (char*)palloc->data(), pos, palloc->size() ) != (word32_t)palloc->size() )
-      return EACCES;
+      if ( PosGet( (char*)palloc->data(), pos, palloc->size() ) != (word32_t)palloc->size() )
+        return EACCES;
 
-    return ((*buf = palloc.ptr())->Attach(), 0);
+      return ((*buf = palloc.ptr())->Attach(), 0);
+    }
+    catch ( const std::bad_alloc& xp  ) {  return error()( ENOMEM, xp );  }
+    catch ( const std::exception& xp )  {  return error()( EFAULT, xp );  }
   }
 
-  word32_t  ZipStream::PosGet( void* ptr, int64_t pos, word32_t len )
+  template <class error>
+  word32_t  ZipStream<error>::PosGet( void* ptr, int64_t pos, word32_t len )
   {
-    std::lock_guard<std::mutex> locker( gzlock );
-
-    if ( gzfile == nullptr )
-      throw std::logic_error( "uninitialized zip stream" );
-    return gzseek( gzfile, (z_off_t)pos, SEEK_SET ) == pos ? (word32_t)gzread( gzfile, ptr, len ) : (word32_t)-1;
+    return interlocked( make_unique_lock( gzlock ), [&]()
+      {  return gzseek( gzfile, (z_off_t)pos, SEEK_SET ) == pos ?
+        (word32_t)gzread( gzfile, ptr, len ) : (word32_t)-1;  } );
   }
 
-  word32_t  ZipStream::PosPut( const void* ptr, int64_t pos, word32_t len )
+  template <class error>
+  word32_t  ZipStream<error>::PosPut( const void* ptr, int64_t pos, word32_t len )
   {
-    std::lock_guard<std::mutex> locker( gzlock );
-
-    if ( gzfile == nullptr )
-      throw std::logic_error( "uninitialized zip stream" );
-    return gzseek( gzfile, (z_off_t)pos, SEEK_SET ) == pos ? (word32_t)gzwrite( gzfile, ptr, len ) : (word32_t)-1;
+    return interlocked( make_unique_lock( gzlock ), [&]()
+      {  return gzseek( gzfile, (z_off_t)pos, SEEK_SET ) == pos ?
+        (word32_t)gzwrite( gzfile, ptr, len ) : (word32_t)-1;  } );
   }
 
-  int64_t   ZipStream::Seek( int64_t pos )
+  template <class error>
+  int64_t   ZipStream<error>::Seek( int64_t pos )
   {
-    std::lock_guard<std::mutex> locker( gzlock );
-
-    if ( gzfile == nullptr )
-      throw std::logic_error( "uninitialized zip stream" );
-    return gzseek( gzfile, (z_off_t)pos, SEEK_SET );
+    return interlocked( make_unique_lock( gzlock ), [&]()
+      {  return gzseek( gzfile, (z_off_t)pos, SEEK_SET );  } );
   }
 
-  int64_t   ZipStream::Size()
+  template <class error>
+  int64_t   ZipStream<error>::Tell()
   {
-    return -1;
+    return interlocked( make_unique_lock( gzlock ), [&]()
+      {  return gztell( gzfile );  } );
   }
 
-  int64_t   ZipStream::Tell()
-  {
-    std::lock_guard<std::mutex> locker( gzlock );
+  // exception processors
 
-    if ( gzfile == nullptr )
-      throw std::logic_error( "uninitialized zip stream" );
-    return gztell( gzfile );
-  }
-
-  IFlatStream*  OpenZipStream( const char* szpath, unsigned dwmode, unsigned buflen )
+  struct report_error_no_except
   {
-    api<ZipStream>  palloc;
+    template <class result, class except>
+    result  operator ()( result res, const except& )  {   return res;  }
+  };
+
+  struct report_error_exception
+  {
+    template <class result, class except>
+    result  operator ()( const result&, const except& x )  {  throw x;  }
+  };
+
+  // creators
+
+  template <class error>
+  auto  openStream( const char* szpath, unsigned dwmode, unsigned buflen ) -> api<ZipStream<error>>
+  {
     gzFile          thezip;
-    char            szmode[8];
-    char*           pszmod = szmode;
+    char            szmode[3] = { 0, 'b', 0 };
 
     switch ( dwmode & 0x000f )
     {
-      case O_RDONLY:  *pszmod++ = 'r';  break;
-      case O_WRONLY:  *pszmod++ = 'w';  break;
-      case O_APPEND:  *pszmod++ = 'a';  break;
-      default:        return nullptr;
+      case O_RDONLY:  szmode[0] = 'r';  break;
+      case O_WRONLY:  szmode[0] = 'w';  break;
+      case O_APPEND:  szmode[0] = 'a';  break;
+      default:        return error()( nullptr, std::invalid_argument( "unsupported access mode" ) );
     }
 
-    *pszmod++ = 'b';  *pszmod++ = '\0';
-
     if ( (thezip = gzopen( szpath, szmode )) == nullptr )
-      return nullptr;
+      return error()( nullptr, file_error( "file not found or invalid zip file" ) );
 
-    if ( (palloc = allocate<ZipStream>( thezip )) == nullptr )  gzclose( thezip );
-      else  gzbuffer( thezip, buflen <= 0x400 ? 0x400 : buflen );
-
-    return palloc.ptr();
+    try
+    {
+      gzbuffer( thezip, buflen <= 0x400 ? 0x400 : buflen );
+      return new ZipStream<error>( thezip );
+    }
+    catch ( const std::bad_alloc& xp )
+      {  gzclose( thezip );  return error()( nullptr, xp );  }
+    catch ( ... )
+      {  gzclose( thezip );  return error()( nullptr, std::current_exception() );  }
   }
 
-  IByteBuffer*  LoadZipBuffer( const char* sz )
+  template <class error>
+  auto  loadBuffer( const char* sz ) -> api<IByteBuffer>
   {
-    api<ZipBuffer>    buffer;
     api<IFlatStream>  infile;
+    api<ZipBuffer>    buffer;
     char              zipbuf[0x400 * 0x40];
     word32_t          cbread;
 
-    if ( (infile = OpenZipStream( sz, O_RDONLY )) == nullptr )
+    if ( (infile = openStream<error>( sz, O_RDONLY, 0x400 * 0x400 )) == nullptr )
       return nullptr;
 
-    if ( (buffer = allocate<ZipBuffer>( 0 )) == nullptr )
-      return nullptr;
-
-    while ( (cbread = infile->Get( zipbuf, sizeof(zipbuf) )) != (word32_t)-1 )
+    try
     {
-      buffer->insert( buffer->end(), zipbuf, zipbuf + cbread );
+      buffer = new ZipBuffer( 0 );
 
-      if ( cbread < sizeof(zipbuf) )
-        return buffer.ptr();
+      while ( (cbread = infile->Get( zipbuf, sizeof(zipbuf) )) != (word32_t)-1 )
+      {
+        buffer->insert( buffer->end(), zipbuf, zipbuf + cbread );
+
+        if ( cbread < sizeof(zipbuf) )
+          return buffer.ptr();
+      }
+
+      return buffer.ptr();
     }
-
-    return nullptr;
+    catch ( const std::bad_alloc& xp )
+      {  return error()( nullptr, xp );  }
+    catch ( ... )
+      {  return error()( nullptr, std::current_exception() );  }
   }
 
-}
+  auto  OpenStream( const char* sz, unsigned mode, const enable_exceptions_t&, unsigned buflen ) -> api<IFlatStream>
+    {  return openStream<report_error_exception>( sz, mode, buflen ).ptr();  }
+  auto  OpenStream( const char* sz, unsigned mode, const disable_exceptions_t&, unsigned buflen ) -> api<IFlatStream>
+    {  return openStream<report_error_no_except>( sz, mode, buflen ).ptr();  }
+
+  auto  OpenStream( const std::string& sz, unsigned mode, const enable_exceptions_t&, unsigned buflen ) -> api<IFlatStream>
+    {  return openStream<report_error_exception>( sz.c_str(), mode, buflen ).ptr();  }
+  auto  OpenStream( const std::string& sz, unsigned mode, const disable_exceptions_t&, unsigned buflen ) -> api<IFlatStream>
+    {  return openStream<report_error_no_except>( sz.c_str(), mode, buflen ).ptr();  }
+
+  auto  LoadBuffer( const char* sz, const enable_exceptions_t& ) -> api<IByteBuffer>
+    {  return loadBuffer<report_error_exception>( sz );  }
+  auto  LoadBuffer( const char* sz, const disable_exceptions_t& ) -> api<IByteBuffer>
+    {  return loadBuffer<report_error_no_except>( sz );  }
+
+  auto  LoadBuffer( const std::string& sz, const enable_exceptions_t& ) -> api<IByteBuffer>
+    {  return loadBuffer<report_error_exception>( sz.c_str() );  }
+  auto  LoadBuffer( const std::string& sz, const disable_exceptions_t& ) -> api<IByteBuffer>
+    {  return loadBuffer<report_error_no_except>( sz.c_str() );  }
+
+}}
